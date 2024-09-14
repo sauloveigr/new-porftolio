0bb6662f3004ac20a49b370a016748ce198fa158
Saulo <sauloveigr@hotmail.com>
2024-09-14 16:44:50 -0300

    feat: implement HomeIntroduction

---
+++ src/components/HomePage/HomeIntroduction.tsx
@@ -0,0 +1,9 @@
+export const HomeIntroduction = () => (
+   <div className="flex flex-col gap-5">
+      <span className="text-xl">Hello, I am</span>
+      <span className="text-7xl leading-none">Saulo Veiga</span>
+      <span className="text-xl text-primary-light-green mt-3">
+         &gt; Front-end Developer
+      </span>
+   </div>
+);
