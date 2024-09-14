7a69b0e1d497958009e1869ee457aa6db89db71b
Saulo <sauloveigr@hotmail.com>
2024-09-14 16:27:32 -0300

    feat: implement HomeBanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Footer } from "./components/Footer";
+import { HomeBanner } from "./components/HomePage/HomeBanner";
+import { NavBar } from "./components/NavBar";
+
 function App() {
-   return <></>;
+   return (
+      <section className="w-full h-screen grid p-2 bg-primary-strong-blue  bg-[url('./assets/bg-blur.svg')] sm:bg-[url('./assets/blur-desktop.svg')] bg-cover lg:bg-auto lg:bg-no-repeat lg:bg-right">
+         <article className="border-2 border-primary-shadow-green rounded-lg relative">
+            <NavBar />
+            <HomeBanner />
+            <Footer />
+         </article>
+      </section>
+   );
 }
 
 export default App;
---
+++ src/components/HomePage/HomeBanner.tsx
@@ -0,0 +1,25 @@
+import { GithubLink } from "./GithubLink";
+import { HomeIntroduction } from "./HomeIntroduction";
+import codeImage from "../../assets/code-snippet-2.svg";
+
+export const HomeBanner = () => {
+   return (
+      <div className=" grid grid-flow-col p-9 pt-24 max-h-[800px] content-around max-w-6xl md:mt-16 m-auto lg:gap-20 place-items-center">
+         <div className="grid gap-20">
+            <HomeIntroduction />
+            <GithubLink />
+         </div>
+         <div className="hidden lg:grid gap-5">
+            {Array(3)
+               .fill(null)
+               .map((_, index: number) => (
+                  <img
+                     key={index}
+                     src={codeImage}
+                     className={index % 2 === 0 ? "opacity-35" : ""}
+                  />
+               ))}
+         </div>
+      </div>
+   );
+};
